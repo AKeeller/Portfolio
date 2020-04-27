b635d9a2f51ab46cedd9adb23f8a863f94fe449b
Alberto Amoruso <alberto.am@me.com>
2020-04-27 18:00:21 +0200

    Added social media icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+import {
+  faFacebook, faTwitter, faTelegram, faFacebookMessenger
+} from '@fortawesome/free-brands-svg-icons';
+
+import {
+  faEnvelope
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Personal-Website';
+
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faTelegram = faTelegram;
+  faFacebookMessenger = faFacebookMessenger;
+  faEnvelope = faEnvelope;
 }
