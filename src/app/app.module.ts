4c3717820f2f99e1b91232d45c3f7a6f9bc87edf
Alberto Amoruso <alberto.am@me.com>
2020-05-06 13:40:53 +0200

    Added Angular Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
+    AppComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FontAwesomeModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
