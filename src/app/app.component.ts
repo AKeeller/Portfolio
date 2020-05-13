c8853f9864c395c8271b0a537a87b24781ca71b3
Alberto Amoruso <alberto.am@me.com>
2020-05-14 00:58:51 +0200

    iOS Web App now has black status bar for dark mode and white status bar for light mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent { }
+
+export class AppComponent implements OnInit {
+
+  constructor(private meta: Meta) { }
+
+  ngOnInit() {
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(e => {
+      this.setColorScheme(e.matches ? 'dark' : 'light');
+    })
+  }
+
+  setColorScheme(scheme: 'dark' | 'light') {
+    if (scheme == 'dark')
+      this.meta.updateTag({ name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' });
+    else
+      this.meta.updateTag({ name: 'apple-mobile-web-app-status-bar-style', content: 'default' });
+  }
+
+}
