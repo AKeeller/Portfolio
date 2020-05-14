cf5be56897ffffbd96b6d179faee8527e0c8a813
Alberto Amoruso <alberto.am@me.com>
2020-05-14 13:28:14 +0200

    Added CSS to About Component

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -3,7 +3,8 @@ import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'about[title]',
-  templateUrl: './about.component.html'
+  templateUrl: './about.component.html',
+  styleUrls: ['about.component.css']
 })
 export class AboutComponent {
 
