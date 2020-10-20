3e49debe5fe0f2a374e9c826d42a96f7b45dfe83
Alberto Amoruso <alberto.am@me.com>
2020-10-21 00:21:57 +0200

    Improved RatingComponent + It now supports half stars

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
-import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as fasStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'rating[stars]',
@@ -16,10 +16,21 @@ export class RatingComponent {
   icons: IconDefinition[] = []
 
   ngOnInit() {
-    for (let i = 0; i < this.stars && i < 5; i++)
+    let maxStars = 5
+
+    if (this.stars > maxStars)
+      this.stars = maxStars
+
+    else if (this.stars < 0)
+      this.stars = 0
+
+    for (let i = 0; i < Math.floor(this.stars); i++)
       this.icons.push(fasStar)
 
-    for (let i = 0; i < 5 - this.stars; i++)
+    if (this.stars % 1 != 0)
+      this.icons.push(faStarHalfAlt)
+
+    for (let i = 0; i < maxStars - Math.ceil(this.stars); i++)
       this.icons.push(farStar)
   }
 
