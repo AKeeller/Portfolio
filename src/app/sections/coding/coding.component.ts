5c6eac3734bb6662f6f04d4067432fcbe507069d
Alberto Amoruso <alberto.am@me.com>
2020-12-19 12:18:24 +0100

    Added links to programming languages in the Skills section

--- src/app/sections/coding/coding.component.ts
+++ src/app/sections/coding/coding.component.ts
@@ -29,20 +29,20 @@ export class CodingComponent {
   faFeatherAlt = faFeatherAlt
   faPhotoVideo = faPhotoVideo
 
-  languages = {
-    "Bash": 4,
-    "C": 4,
-    "C#": 3,
-    "Erlang": 2,
-    "F#": 2,
-    "Java": 4,
-    "LaTeX": 3,
-    "Objective-C": 3,
-    "OCaml": 2,
-    "PHP": 1,
-    "Scala": 2,
-    "Swift": 4.5,
-    "TypeScript": 3.5
-  }
+  languages: [string, number, URL][] = [
+    ["Bash", 4, new URL("https://www.gnu.org/software/bash/")],
+    ["C", 4, new URL("https://www.gnu.org/software/gnu-c-manual/")],
+    ["C#", 3, new URL("https://docs.microsoft.com/dotnet/csharp/")],
+    ["Erlang", 2, new URL("https://www.erlang.org")],
+    ["F#", 2, new URL("https://fsharp.org")],
+    ["Java", 4, new URL("https://java.com")],
+    ["LaTeX", 3, new URL("https://www.latex-project.org")],
+    ["OCaml", 2, new URL("https://ocaml.org")],
+    ["Objective-C", 3, new URL("https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/Introduction/Introduction.html")],
+    ["PHP", 1, new URL("https://www.php.net")],
+    ["Scala", 2, new URL("https://www.scala-lang.org")],
+    ["Swift", 4.5, new URL("https://developer.apple.com/swift/")],
+    ["TypeScript", 3.5, new URL("https://www.typescriptlang.org")]
+  ]
 
 }
