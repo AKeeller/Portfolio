8f58471acf05f23806ede6e21b1343065ecb73b5
Alberto Amoruso <alberto.am@me.com>
2025-07-17 19:11:08 +0200

    style(newline): formatting

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ function calculateAge(birthdate) {
     var diff = Date.now() - birth;
     return Math.floor(diff / 1000 / 60 / 60 / 24 / 365.256);
 }
+
 document.addEventListener("DOMContentLoaded", function () {
     var ageSpan = document.getElementById("age");
     if (ageSpan) {
