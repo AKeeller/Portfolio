e4a9a2d9f949dc6b92ba21abcc4c866b7921265f
Alberto Amoruso <alberto.am@me.com>
2025-07-21 19:16:13 +0200

    feat(404): add error page

---
+++ src/js/404.js
@@ -0,0 +1,8 @@
+import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
+
+new DotLottie({
+	autoplay: true,
+	loop: true,
+	canvas: document.getElementById("canvas"),
+	src: "https://assets-v2.lottiefiles.com/a/39cee570-1164-11ee-ab76-7fb431ce1557/vj2WeIBKFH.lottie"
+});
